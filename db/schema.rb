54663b45cc4816e6a8da1d55cd4f38bde0fcd9e2
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-23 08:56:11 -0700

    Session added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_154021) do
+ActiveRecord::Schema.define(version: 2020_02_23_155337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login"
