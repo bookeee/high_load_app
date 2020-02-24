0e2eba21f9882dd607fbd3f0104a2d21d1ac89b7
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-24 11:01:02 -0700

    errors handlig added to application controlller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :unexpected_error
+
+  def unexpected_error(e)
+    Rails.logger.error(e.message)
+    render_error_response(e.message, 500)
+  end
+
+  def render_error_response(message, status)
+    render json: { error: message }, status: status
+  end
 end
