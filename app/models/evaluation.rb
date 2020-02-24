7771bf4168b8018be3b46be1262498d44442b73b
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-24 11:08:50 -0700

    pervious_by_request scope added (should get all evaluations which are created before request was made)

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -5,4 +5,10 @@ class Evaluation < ApplicationRecord
 
   validates_presence_of :value
   validates_inclusion_of :value, in: 1..5
+
+  scope :previous_by_request, ->(timestamp) { where('created_at <= ?', timestamp) }
+
+  def rating
+    Rating.where(last_est_id: id).first
+  end
 end
