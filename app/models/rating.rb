b7f7472d435535759edcffe617b78488cdfdf8f8
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-24 11:09:31 -0700

    validations corrected + method to get readable avg

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,9 @@
 class Rating < ApplicationRecord
   belongs_to :statistics
 
-  validates_presence_of :average, :est_amount, :values_sum, :last_est_id
+  validates_presence_of :average, :est_amount, :values_sum, :last_est_time, :last_est_id
+
+  def formatted_avg
+    average.to_s
+  end
 end
