02b0c14a5a00375f27de7a30c39ce2dfafa53a1c
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-25 17:30:20 -0700

    method to check if another user used same ip during potst#create

--- app/models/persona/session.rb
+++ app/models/persona/session.rb
@@ -3,5 +3,9 @@
 module Persona
   class Session < ApplicationRecord
     has_and_belongs_to_many :users
+
+    def self.used_by_another_user?(ip, user_id)
+      Queries::Sessions::UsedByOtherUsersQuery.new(ip, user_id).call.any?
+    end
   end
 end
