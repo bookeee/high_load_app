af199e9c82563481683458a59e34c6fa52d9392c
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-25 17:40:46 -0700

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:create]
+      get "/posts/:posts_amount" => "posts#top"
       post "/evaluations" => "evaluations#do"
+      resources :matches, only: [:index]
     end
   end
 
