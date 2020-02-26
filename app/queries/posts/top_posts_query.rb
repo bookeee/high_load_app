bb895bd9e24be18a91cf91062e773c9fc60eea05
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-26 13:11:45 -0700

    top posts query service added

---
+++ app/queries/posts/top_posts_query.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module Queries
+  module Posts
+    class TopPostsQuery
+      KEY_NAME = 'top_posts_query'
+
+      include Cacheable
+      include Serializable
+
+      attr_reader :amount
+
+      def initialize(amount, posts_ids)
+        @amount = amount
+        @posts_ids = posts_ids
+      end
+
+      def call
+        if amount_cached? && amount_can_be_cached?(KEY_NAME)
+          get_records_from_cache
+        else
+          set_key
+          get_records_from_cache
+        end
+      end
+
+      private
+
+      def set_key
+        delete_(KEY_NAME)
+        posts = serialize(records_array)
+        create_list(KEY_NAME, posts)
+        set_expiration_time(KEY_NAME)
+      end
+
+      def get_records_from_cache
+        deserialize(get_key)
+      end
+
+      def get_key
+        get_list(KEY_NAME, 0, ends_at)
+      end
+
+      def ends_at
+        amount - 1
+      end
+
+      def amount_cached?
+        list_count(KEY_NAME) >= amount
+      end
+
+      def sql
+        <<-SQL
+          SELECT *
+          FROM   unnest('{#{formatted_ids}}'::int[]) "id"
+          JOIN   posts "Post" USING ("id");
+        SQL
+      end
+
+      def records_array
+        perform_query.to_a
+      end
+
+      def perform_query
+        ActiveRecord::Base.connection.execute(sql)
+      end
+
+      def formatted_ids
+        @posts_ids.join(',')
+      end
+    end
+  end
+end
