0f7737a38070aa67d8e1489e8adf613cc90ee6da
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-26 14:20:52 -0700

    seeds.rb updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def value
+  [1,2,3,4,5].sample
+end
+
+posts_amount = 300
+evaluations_amount = 200
+
+posts_amount.times do |n|
+  puts "Iteration nubmer #{n}"
+  resp = Faraday.post('http://localhost:3000/api/v1/posts') do |req|
+    req.headers['Content-Type'] = 'application/json'
+    req.body = { post: {title: "test_post_title#{n}",
+                        content: "test content",
+                        ip:  "222.444.111.#{n}",
+                        login: "test_login_#{n}" } }.to_json
+  end
+
+  if n < evaluations_amount
+    resp = Faraday.post('http://localhost:3000/api/v1/evaluations') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { evaluation: {post_id: "#{n}",
+                                value: value} }.to_json
+    end
+  end
+
+end
+
+# in seconds
+top_posts_caching_time = 600
+# object count
+top_posts_limit = 210000
+
+Settings::Caching.create!(top_posts_caching_time: top_posts_caching_time, top_posts_limit: top_posts_limit)
