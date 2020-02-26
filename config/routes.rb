368de1f34ab04822bb751f31c15cc3bbbca91b1d
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-02-26 14:21:28 -0700

    posts route changed to top_posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
+  
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:create]
-      get "/posts/:posts_amount" => "posts#top"
+      get "/top_posts/:posts_amount" => "posts#top"
       post "/evaluations" => "evaluations#do"
       resources :matches, only: [:index]
     end
