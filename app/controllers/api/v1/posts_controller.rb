3a74017a8e43631ff0efcd5980c2af30a00a0be6
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-03-03 13:43:46 -0700

    posts controller updated, codes to responses added

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,19 +6,19 @@ module Api
       def create
         @post = Services::Posts::Create.new(post_params).call
 
-        if @post.persisted?
-          render json: PostPresenter.new(@post), status: :ok
+        if @post.try(:persisted?)
+          render json: { code: 200, post: Posts::PostPresenter.new(@post) }, status: :ok
         else
-          render_error_response(@post.errors.messages, 422)
+          render_error_response(@post, 422)
         end
       end
 
       def top
         @posts = Services::Posts::Top.new(params[:posts_amount]).call
-        if @posts.present?
-          render json: PostsPresenter.new(@posts)
+        if @posts.present? && !@posts.is_a?(Exception)
+          render json: { code: 200, posts: Posts::PostsPresenter.new(@posts) }
         else
-          render_error_response(@post.errors.messages, 422)
+          render_error_response(@posts, 422)
         end
       end
 
