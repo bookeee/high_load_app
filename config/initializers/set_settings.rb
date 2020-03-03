c09cd4e25cae7d6884a271d4850097c6813dc021
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-03-03 13:50:38 -0700

    SettingsSetup logic changed

--- config/initializers/set_settings.rb
+++ config/initializers/set_settings.rb
@@ -1,4 +1,13 @@
-Settings::Caching.delete_all
-Settings::Caching.create!(top_posts_caching_time: ENV["TOP_POSTS_CACHING_TIME"], top_posts_limit: ENV["TOP_POSTS_LIMIT"])
+def setup
+  Settings::Caching.delete_all
+  Settings::Caching.create!(top_posts_caching_time: ENV["TOP_POSTS_CACHING_TIME"], top_posts_limit: ENV["TOP_POSTS_LIMIT"])
+rescue ActiveRecord::StatementInvalid => e
+  if e.cause.is_a?(PG::UndefinedTable)
+    puts "You should define tables (rake db:migrate) first."
+  end
+end
 
 
+if Rails.env.development?
+  setup
+end
