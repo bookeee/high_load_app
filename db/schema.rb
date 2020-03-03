e9d41fedcc17ccf2a78892888b184a1b4de7bad4
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-03-03 13:54:59 -0700

    schema rb commited for demonstration purposes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_200605) do
+ActiveRecord::Schema.define(version: 2020_02_26_021257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cachings", force: :cascade do |t|
+    t.integer "top_posts_caching_time"
+    t.integer "top_posts_limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "evaluations", force: :cascade do |t|
     t.integer "value"
