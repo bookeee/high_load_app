33341a6266e26b95931e9f32a0cf84bfb3549e5f
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-03-03 13:50:13 -0700

    redis setup for test env added

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis::Namespace.new("high_load_app", :redis => Redis.new)
+unless Rails.env.test?
+  $redis = Redis::Namespace.new("high_load_app", redis: Redis.new )
+else
+  $redis = Redis::Namespace.new("high_load_app", redis: MockRedis.new )
+end
