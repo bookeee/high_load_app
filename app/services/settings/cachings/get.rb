85dd51103afcd3c53eafbf08377a1a41e86f928c
alex223124 <alexalexalexalexalexalexalex@alexalex.com>
2020-03-03 13:53:17 -0700

    constant name rewritten to FIELDS_MAPPING,  make more sence

--- app/services/settings/cachings/get.rb
+++ app/services/settings/cachings/get.rb
@@ -4,8 +4,8 @@ module Services
   module Settings
     module Cachings
       class Get
-        SETTINGS_MAPPING = { top_posts_query: { caching_time: 'top_posts_caching_time',
-                                                limit: 'top_posts_limit' } }.freeze
+        FIELDS_MAPPING = { top_posts_query: { caching_time: 'top_posts_caching_time',
+                                              limit: 'top_posts_limit' } }.freeze
 
         def self.call(query, setting_name)
           settings.public_send(field(query, setting_name))
@@ -16,7 +16,7 @@ module Services
         end
 
         def self.field(query, setting_name)
-          SETTINGS_MAPPING[query.to_sym][setting_name.to_sym]
+          FIELDS_MAPPING[query.to_sym][setting_name.to_sym]
         end
       end
     end
